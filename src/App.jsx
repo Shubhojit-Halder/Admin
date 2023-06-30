39d41587eebc0ee525d34f572f151a9d0f8e0fa9
Shubhojit Halder <shubhojit_h.ece2020@msit.edu.in>
2023-06-30 12:47:37 +0530

    Changes in main file

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,29 @@
-import "./App.css";
-import Home from "./pages/Home";
+import { useSelector } from "react-redux";
+import Home from "./Pages/Home";
 
-function App() {
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Navigate,
+} from "react-router-dom";
+import axios from "axios";
 
+const App = () => {
+  const user = useSelector((state) => state.user.currentUser);
   return (
-    <>
-      <Home />
-    </>
+    <Router>
+      <Routes>
+        <Route exact path="/dashboard" element={<Home />} />
+
+        <Route
+          exact
+          path="/"
+          element={user !== null ? <Login /> : <Navigate to="/dashboard" />}
+        />
+      </Routes>
+    </Router>
   );
-}
+};
 
 export default App;
